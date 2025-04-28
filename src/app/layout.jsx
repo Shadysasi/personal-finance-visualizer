ba8e881c51559afa9f5aac194927b371fdee805d
Shadysasi <sasieng5851@gmail.com>
2025-04-28 19:02:56 +0530

    first commits

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,6 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Personal Finance Visualizer",
-  description: "Created by Rajdp-krmkr(Github)",
 };
 
 export default function RootLayout({ children }) {
